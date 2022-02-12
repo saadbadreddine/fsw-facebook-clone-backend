c449a548b244eee2a8845df1f75cf68a402612f3
Saad <badreddinesaad91@gmail.com>
2022-02-12 21:39:39 +0200

    Add Friend API: Querried the Database to insert a tupple inside the friendships table

--- addfr_api.php
+++ addfr_api.php
@@ -2,11 +2,21 @@
 
 include("db_info.php");
 
+$sender_id = 1;
+$receiver_id = 2;
+$accepted = false;
 
+$query = $mysqli -> prepare("INSERT INTO friendships(sender_id, receiver_id, accepted) VALUES (:sender_id, :receiver_id, :accepted)"); 
+$query->bind_param("s", $sender_id);
+$query->bind_param("s", $receiver_id);
+$query->bind_param("b", $accepted);
+$query->execute();
 
+$array_response = [];
+$array_response["status"] = "Friend Request Sent";
 
-
-
+$json_response = json_encode($array_response);
+echo $json_response;
 
 
 ?>
