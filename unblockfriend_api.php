03ee01a0011efc661c5a3fbd289853b7b1991af7
Saad <badreddinesaad91@gmail.com>
2022-02-13 01:31:44 +0200

    Unblock API : Querries the Database to delete the row with the corresponding values from blocks table

---
+++ unblockfriend_api.php
@@ -0,0 +1,21 @@
+<?php
+
+include("db_info.php");
+
+$sender_id = 1;
+$receiver_id = 2;
+
+$unblock = true;
+
+$query = $mysqli -> prepare("DELETE FROM blocks WHERE (sender = ? AND receiver = ?)"); 
+$query->bind_param("ii", $sender_id, $receiver_id);
+$query->execute();
+
+$array_response = [];
+$array_response["status"] = "User Unblocked";
+$json_response = json_encode($array_response);
+echo $json_response;
+
+
+
+?>
