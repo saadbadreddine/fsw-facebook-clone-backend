72cfae680257037a39ec7c69d462a5cd8ca9f133
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 22:07:30 +0200

    Decode JWT and edit query and array_response

--- PHP/status-api.php
+++ PHP/status-api.php
@@ -3,6 +3,14 @@
 include("db_info.php");
 include("authorization_api.php");
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+$decoded_key = JWT::decode($jwt, new Key($key, 'HS256'));
+
+if(isset($_POST[$decoded_key])){
+    $user = $_POST[$decoded_key];
+}else{
+    die("User not found");
+}
 
 // Add a status(post)
 
@@ -12,18 +20,12 @@ if (empty($_POST["post"])) {
     $post = $_POST["post"];
 }
 
-if(isset($_POST["user_id"])){
-    $user_id = $_POST["user_id"];
-}else{
-    die("User not found");
-}
-
 $query = $mysqli->prepare("INSERT INTO posts(post, user_id) VALUES (?, ?)"); 
-$query->bind_param("si", $post, $user_id);
+$query->bind_param("si", $post, $user);
 $query->execute();
 
 $array_response = [];
-$array_response["status"] = $post;
+$array_response = ["status" => "Post added", "post" => $post];
 
 $json_response = json_encode($array_response);
 echo $json_response;
