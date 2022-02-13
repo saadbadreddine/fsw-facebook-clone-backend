9f54f8f462436b4db507a15b585c19123c5dea0d
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 03:21:04 +0200

    Edit mysqli prepare function

--- PHP/friend-status-api.php
+++ PHP/friend-status-api.php
@@ -4,13 +4,13 @@ include("db_info.php");
 
 $post = $_POST["post"];
 $user_id = $_POST["id"];
-
+$blocked = false;
 $query = $mysqli->prepare("SELECT posts.post, users.first_name, users.last_name 
                             FROM posts JOIN users ON posts.user_id = ?
                             JOIN friendships ON (friendships.sender = ?) OR (friendships.receiver = ?) 
-                            WHERE users.blocked = false"); 
+                            WHERE friendships.blocked = ?"); 
                            
-$query->bind_param("iii",$user_id, $user_id, $user_id);
+$query->bind_param("iiib",$user_id, $user_id, $user_id, $blocked);
 $query->execute();
 
 $array_response = [];
