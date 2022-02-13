2cc78b3e571d2149382c2434807d54cfe81970a0
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 03:30:24 +0200

    Implement profilepic-api

---
+++ PHP/profilepic-api.php
@@ -0,0 +1,18 @@
+<?php
+
+include("db_info.php");
+
+$picture = $_POST["picture"];
+$user_id = $_POST["id"];
+
+$query = $mysqli->prepare("INSERT INTO users picture=? WHERE users.id = ?"); 
+$query->bind_param("si", $picture, $user_id);
+$query->execute();
+
+$array_response = [];
+$array_response["status"] = $picture;
+
+$json_response = json_encode($array_response);
+echo $json_response;
+
+?>
