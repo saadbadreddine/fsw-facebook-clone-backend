7ba0d543e17bb8838cd705d705554105e23988aa
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 16:17:36 +0200

    Implement validation

--- PHP/status-api.php
+++ PHP/status-api.php
@@ -4,8 +4,17 @@ include("db_info.php");
 
 // Add a status(post)
 
-$post = $_POST["post"];
-$user_id = $_POST["user_id"];
+if (empty($_POST["post"])) {
+    die("Post is empty");
+}else{
+    $post = $_POST["post"];
+}
+
+if(isset($_POST["user_id"])){
+    $user_id = $_POST["user_id"];
+}else{
+    die("User not found");
+}
 
 $query = $mysqli->prepare("INSERT INTO posts(post, user_id) VALUES (?, ?)"); 
 $query->bind_param("si", $post, $user_id);
