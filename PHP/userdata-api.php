4bda9eabcf4e1316ff73fa346643472b55603455
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 16:15:47 +0200

    Implement validation

--- PHP/userdata-api.php
+++ PHP/userdata-api.php
@@ -2,8 +2,18 @@
 
 include("db_info.php");
 
-$email = $_POST["email"];
-$password = $_POST["password"];
+if(isset($_POST["email"])){
+    $email = $mysqli->real_escape_string($_POST["email"]);
+}else{
+    die("Email not found");
+}
+
+if(isset($_POST["password"])){
+    $password = $mysqli->real_escape_string($_POST["password"]);
+    $password = hash("sha256", $password);
+}else{
+    die("Wrong Password");
+}
 
 $query = $mysqli->prepare("SELECT first_name, last_name, dob_d, dob_m, dob_y, email, picture, addresses.country, addresses.city, 
                             addresses.street FROM users JOIN addresses ON  users.id = addresses.user_id WHERE email = ? AND password = ?"); 
