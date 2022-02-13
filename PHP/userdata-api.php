00e33cfa659cb95c726d6217d43d731ecd526960
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 21:23:11 +0200

    Edit  into a JWT token and change query and response

--- PHP/userdata-api.php
+++ PHP/userdata-api.php
@@ -3,27 +3,22 @@
 include("db_info.php");
 include("authorization_api.php");
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+$decoded_key = JWT::decode($jwt, new Key($key, 'HS256'));
 
-if(isset($_POST["email"])){
-    $email = $mysqli->real_escape_string($_POST["email"]);
+if(isset($_POST[$decoded_key])){
+    $user = $_POST[$decoded_key];
 }else{
-    die("Email not found");
-}
-
-if(isset($_POST["password"])){
-    $password = $mysqli->real_escape_string($_POST["password"]);
-    $password = hash("sha256", $password);
-}else{
-    die("Wrong Password");
+    die("User not found");
 }
 
 $query = $mysqli->prepare("SELECT first_name, last_name, dob_d, dob_m, dob_y, email, picture, addresses.country, addresses.city, 
-                            addresses.street FROM users JOIN addresses ON  users.id = addresses.user_id WHERE email = ? AND password = ?"); 
-$query->bind_param("ss", $email, $password);
+                            addresses.street FROM users JOIN addresses ON  users.id = addresses.user_id WHERE id = ?"); 
+$query->bind_param("i", $decoded_key);
 $query->execute();
 
 $array_response = [];
-$array_response["status"] = $query;
+$array_response = ["status" => "Get user data", $query];
 
 $json_response = json_encode($array_response);
 echo $json_response;
