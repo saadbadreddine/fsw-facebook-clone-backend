739fa705c579e8c1b3aaff5a18e096917b8c2334
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 12:26:18 +0200

    Implement the count likes-api

---
+++ PHP/likes-api.php
@@ -0,0 +1,17 @@
+<?php
+
+include("db_info.php");
+
+$post = $_POST["post_id"];
+
+$query = $mysqli->prepare("SELECT COUNT(post_id) FROM likes WHERE post_id = ?"); 
+$query->bind_param("ii", $post);
+$query->execute();
+
+$array_response = [];
+$array_response["status"] = $query;
+
+$json_response = json_encode($array_response);
+echo $json_response;
+
+?>
