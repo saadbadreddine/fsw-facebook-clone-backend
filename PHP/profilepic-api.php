fec7bce431b1ee09021adc972be8e68705018e8b
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 21:08:18 +0200

    Decode JWT token and edit array_response

--- PHP/profilepic-api.php
+++ PHP/profilepic-api.php
@@ -3,13 +3,14 @@
 include("db_info.php");
 include("authorization_api.php");
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+$decoded_key = JWT::decode($jwt, new Key($key, 'HS256'));
 
 // Validate Picture
 $picture = $_POST["picture"];
 
-
 if(isset($_POST["id"])){
-    $user_id = $_POST["id"];
+    $user_id = $_POST[$decoded_key];
 }else{
     die("User not found");
 }
@@ -18,7 +19,7 @@ $query->bind_param("si", $picture, $user_id);
 $query->execute();
 
 $array_response = [];
-$array_response["status"] = $picture;
+$array_response = ["status" => "Show Picture", "picture" => $picture];
 
 $json_response = json_encode($array_response);
 echo $json_response;
