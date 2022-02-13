913490416be9bdf14639ff5e221a2f0d1fb338d9
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 16:16:07 +0200

    Implement validation

--- PHP/friend-status-api.php
+++ PHP/friend-status-api.php
@@ -2,16 +2,26 @@
 
 include("db_info.php");
 
-$post = $_POST["post"];
-$user_id = $_POST["id"];
+if(isset($_POST["post"])){
+    $post = $_POST["post"];
+}else{
+    die("Post not found");
+}
+
+if(isset($_POST["id"])){
+    $user_id = $_POST["id"];
+}else{
+    die("User not found");
+}
 $blocked = false;
+
 $query = $mysqli->prepare("SELECT posts.post, posts.timestamp, users.id, users.first_name, users.last_name, users.picture 
                             FROM posts JOIN users ON posts.user_id = ?
                             JOIN friendships ON (friendships.sender = ? OR friendships.receiver = ?) 
                             AND friendships.accepted = 1 AND users.id NOT IN (SELECT blocks.sender, blocks.receiver 
                             FROM blocks WHERE blocks.sender = ? OR blocks.receiver = ?)"); 
                            
-$query->bind_param("iiib",$user_id, $user_id, $user_id, $blocked);
+$query->bind_param("iiibb",$user_id, $user_id, $user_id, $blocked, $blocked);
 $query->execute();
 
 $array_response = [];
