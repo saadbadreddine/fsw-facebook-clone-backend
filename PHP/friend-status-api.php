f70e86f026ce72ae09d7d35177dd7082dd041295
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-14 00:17:20 +0200

    Change  to file_get_content

--- PHP/friend-status-api.php
+++ PHP/friend-status-api.php
@@ -4,10 +4,17 @@ include("db_info.php");
 include("authorization_api.php");
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
-$decoded_key = JWT::decode($jwt, new Key($key, 'HS256'));
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-if(isset($_POST[$decoded_key])){
-    $user_id = $_POST[$decoded_key];
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+
+if(isset($data -> id)){
+    $key = JWT::decode($jwt, new Key($key, 'HS256'));
 }else{
     die("User not found");
 }
@@ -19,11 +26,15 @@ $query = $mysqli->prepare("SELECT posts.post, posts.timestamp, users.id, users.f
                             AND friendships.accepted = 1 AND users.id NOT IN (SELECT blocks.sender, blocks.receiver 
                             FROM blocks WHERE blocks.sender = ? OR blocks.receiver = ?)"); 
                            
-$query->bind_param("iiibb",$user_id, $user_id, $user_id, $blocked, $blocked);
+$query->bind_param("iiibb",$key, $key, $key, $blocked, $blocked);
 $query->execute();
 
+$query->store_result;
+$query->bind_result($post);
+$query->fetch();
+
 $array_response = [];
-$array_response = ["status" => "List of posts", "posts" => $query];
+$array_response = ["status" => "List of posts", "posts" => $post];
 
 $json_response = json_encode($array_response);
 echo $json_response;
