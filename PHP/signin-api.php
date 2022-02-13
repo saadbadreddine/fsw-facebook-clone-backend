0186522fc419fbcd192128052fca9a1cfb9215bb
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 23:21:19 +0200

    Change  to file_get_content

--- PHP/signin-api.php
+++ PHP/signin-api.php
@@ -3,18 +3,28 @@
 include("db_info.php");
 include("authorization_api.php");
 use Firebase\JWT\JWT;
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-if(isset($_POST["email"])){
-    $email = $mysqli->real_escape_string($_POST["email"]);
-}else{
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+$email = $data -> email;
+$password = $data -> password;
+
+if(empty($email)){
     die("Please Sign up");
+}else{
+    $email = $mysqli -> real_escape_string($email);
 }
 
-if(isset($_POST["password"])){
-    $password = $mysqli->real_escape_string($_POST["password"]);
-    $password = hash("sha256", $password);
-}else{
+if(empty($password)){
     die("Wrong Password");
+}else{
+    $password = $mysqli->real_escape_string($password);
+    $password = hash("sha256", $password);
 }
 
 $query = $mysqli->prepare("SELECT id FROM users WHERE email = ? And password = ?");
