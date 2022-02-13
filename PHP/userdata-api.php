0b37e191861b82bc29668f6463e3c31a35048788
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-14 00:35:58 +0200

    Change  to file_get_content

--- PHP/userdata-api.php
+++ PHP/userdata-api.php
@@ -4,21 +4,32 @@ include("db_info.php");
 include("authorization_api.php");
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
-$decoded_key = JWT::decode($jwt, new Key($key, 'HS256'));
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-if(isset($_POST[$decoded_key])){
-    $user = $_POST[$decoded_key];
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+
+if(isset($data -> id)){
+    $key = JWT::decode($jwt, new Key($key, 'HS256'));
 }else{
     die("User not found");
 }
 
 $query = $mysqli->prepare("SELECT first_name, last_name, dob_d, dob_m, dob_y, email, picture, addresses.country, addresses.city, 
                             addresses.street FROM users JOIN addresses ON  users.id = addresses.user_id WHERE id = ?"); 
-$query->bind_param("i", $decoded_key);
+$query->bind_param("i", $key);
 $query->execute();
 
+$query->store_result;
+$query->bind_result($data);
+$query->fetch();
+
 $array_response = [];
-$array_response = ["status" => "Get user data","data" => $query];
+$array_response = ["status" => "Get user data","data" => $data];
 
 $json_response = json_encode($array_response);
 echo $json_response;
