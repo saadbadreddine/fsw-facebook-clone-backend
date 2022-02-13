4d126d11876046478bba951c2d36deb5979a29f8
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-13 11:07:06 +0200

    Edit SELECT query

--- PHP/friend-status-api.php
+++ PHP/friend-status-api.php
@@ -5,10 +5,11 @@ include("db_info.php");
 $post = $_POST["post"];
 $user_id = $_POST["id"];
 $blocked = false;
-$query = $mysqli->prepare("SELECT posts.post, users.first_name, users.last_name 
+$query = $mysqli->prepare("SELECT posts.post, users.id, users.first_name, users.last_name, users.picture 
                             FROM posts JOIN users ON posts.user_id = ?
-                            JOIN friendships ON (friendships.sender = ?) OR (friendships.receiver = ?) 
-                            WHERE friendships.blocked = ?"); 
+                            JOIN friendships ON (friendships.sender = ? OR friendships.receiver = ?) 
+                            AND friendships.accepted = 1 AND users.id NOT IN (SELECT blocks.sender, blocks.receiver 
+                            FROM blocks WHERE blocks.sender = ? OR blocks.receiver = ?)"); 
                            
 $query->bind_param("iiib",$user_id, $user_id, $user_id, $blocked);
 $query->execute();
