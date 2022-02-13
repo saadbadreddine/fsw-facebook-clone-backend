dbf1e48bef34ab675cdd6839c8ee76f009678ed2
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-14 00:13:40 +0200

    Change  to file_get_content

--- PHP/edit-status-api.php
+++ PHP/edit-status-api.php
@@ -2,18 +2,20 @@
 
 include("db_info.php");
 include("authorization_api.php");
-use Firebase\JWT\JWT;
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$json = file_get_contents('php://input');
+$data = json_decode($json);
 
 // Edit a status(post)
 
-if (empty($_POST["post"])) {
-    die("Post is empty");
-}else{
-    $updated_post =$_POST["post"];
-}
-
-if(isset($_POST["post_id"])){
-    $post_id= $_POST["post_id"];
+if(isset($data -> post_id)){
+    $post_id = $data -> post_id;
+    $updated_post = $data -> post;
 }else{
     die("Post not found");
 }
@@ -22,8 +24,12 @@ $query = $mysqli->prepare("UPDATE posts SET post=? WHERE post_id=?");
 $query->bind_param("si", $updated_post, $post_id);
 $query->execute();
 
+$query->store_result;
+$query->bind_result($like_id);
+$query->fetch();
+
 $array_response = [];
-$array_response = ["status" => "Status updated", "post" => $updated_post];
+$array_response = ["status" => "Status updated", "like" => $like_id];
 
 $json_response = json_encode($array_response);
 echo $json_response;
