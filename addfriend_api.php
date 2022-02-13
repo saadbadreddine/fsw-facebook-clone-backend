6b0f8f68e2583010bab42f05acc9e74f2d7d945b
Saad <badreddinesaad91@gmail.com>
2022-02-14 01:49:19 +0200

    API can be fetched using POST

--- addfriend_api.php
+++ addfriend_api.php
@@ -1,25 +1,35 @@
 <?php
 
 include("db_info.php");
-require __DIR__ . '/vendor/autoload.php';
 include("authorization_api.php");
-
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+$sender_id  = $data -> sender;
+$receiver_id = $data -> receiver;
 
 
-$jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsb2NhbGhvc3QiLCJhdWQiOiJsb2NhbGhvc3QiLCJpYXQiOjEzNTY5OTk1MjQsIm5iZiI6MTM1NzAwMDAwMCwiZGF0YSI6Nn0.mJVKuGfsYWv60PTTD3apVB7PgSJKXj83PcmVBplnJj4";
-$decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+$decoded_sender = JWT::decode($sender_id, new Key($key, 'HS256'));
+$decoded_sender = $decoded_sender -> id;
+$decoded_receiver = JWT::decode($receiver_id, new Key($key, 'HS256'));
+$decoded_receiver = $decoded_receiver -> id;
 
-print_r($decoded);
-$decoded_array = (array) $decoded;
 
-$sender_id = 1;
-$receiver_id = 2;
+if(empty($data)){
+   die("Error");
+}
+
 $accepted = false;
 
 $query = $mysqli -> prepare("INSERT INTO friendships(sender, receiver, accepted) VALUES (?, ?, ?)"); 
-$query->bind_param("iib", $sender_id, $receiver_id, $accepted);
+$query->bind_param("iib", $decoded_sender, $decoded_receiver, $accepted);
 $query->execute();
 
 $array_response = [];
