07e4b8fe96b4d786296f1346476c865029e8e3d5
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-14 00:35:06 +0200

    Change  to file_get_content

--- PHP/profilepic-api.php
+++ PHP/profilepic-api.php
@@ -4,22 +4,35 @@ include("db_info.php");
 include("authorization_api.php");
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
-$decoded_key = JWT::decode($jwt, new Key($key, 'HS256'));
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// Validate Picture
-$picture = $_POST["picture"];
+$json = file_get_contents('php://input');
+$data = json_decode($json);
 
-if(isset($_POST["id"])){
-    $user_id = $_POST[$decoded_key];
+if(isset($data -> id)){
+    $key = JWT::decode($jwt, new Key($key, 'HS256'));
 }else{
     die("User not found");
 }
+
+// Validate Picture
+
+$picture = $data -> picture;
+
 $query = $mysqli->prepare("UPDATE users  SET picture=? WHERE users.id = ?"); 
-$query->bind_param("si", $picture, $user_id);
+$query->bind_param("si", $picture, $key);
 $query->execute();
 
+$query->store_result;
+$query->bind_result($image);
+$query->fetch();
+
 $array_response = [];
-$array_response = ["status" => "Show Picture", "picture" => $picture];
+$array_response = ["status" => "Show Picture", "picture" => $image];
 
 $json_response = json_encode($array_response);
 echo $json_response;
