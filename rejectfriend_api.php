b955cb12246ffd13ca97c914e1a03f0e0ebb02c2
Saad <badreddinesaad91@gmail.com>
2022-02-13 02:18:19 +0200

    Reject Friend API: Querries the Database and Deletes the row with the matching sender and receiver ids from friendships table

---
+++ rejectfriend_api.php
@@ -0,0 +1,19 @@
+<?php
+
+include("db_info.php");
+
+include("db_info.php");
+
+$sender_id = 1;
+$receiver_id = 2;
+
+$query = $mysqli -> prepare("DELETE FROM friendships WHERE sender = ? AND receiver = ?");
+$query->bind_param("ii", $receiver_id, $sender_id);
+$query->execute();
+
+$array_response = [];
+$array_response["status"] = "Friend Request Rejected";
+$json_response = json_encode($array_response);
+echo $json_response;
+
+?>
