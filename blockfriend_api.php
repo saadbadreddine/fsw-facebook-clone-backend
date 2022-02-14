1ed7dc1249a205f086c0faf451ca793997711641
Saad <badreddinesaad91@gmail.com>
2022-02-14 10:05:37 +0200

    API can be fetched using POST

--- blockfriend_api.php
+++ blockfriend_api.php
@@ -1,19 +1,32 @@
 <?php
 
 include("db_info.php");
+include("authorization_api.php");
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$sender_id = 1;
-$receiver_id = 2;
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+$sender_id  = $data -> sender;
+$receiver_id = $data -> receiver;
+
+$decoded_sender = JWT::decode($sender_id, new Key($key, 'HS256'));
+$decoded_sender = $decoded_sender -> id;
+$decoded_receiver = JWT::decode($receiver_id, new Key($key, 'HS256'));
+$decoded_receiver = $decoded_receiver -> id;
 
 $query = $mysqli -> prepare("INSERT INTO blocks(sender, receiver) VALUES (?, ?)"); 
-$query->bind_param("ii", $sender_id, $receiver_id);
+$query->bind_param("ii", $decoded_sender, $decoded_receiver);
 $query->execute();
 
 $array_response = [];
 $array_response["status"] = "User Blocked";
 $json_response = json_encode($array_response);
 echo $json_response;
-
-
 
 ?>
