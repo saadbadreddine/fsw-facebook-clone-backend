209bd6ad7441f210793a48e52e095ed9b738141e
Saad <badreddinesaad91@gmail.com>
2022-02-14 10:24:16 +0200

    API can be fetched using POST; JWT implemented

--- rejectfriend_api.php
+++ rejectfriend_api.php
@@ -1,12 +1,27 @@
 <?php
 
 include("db_info.php");
+include("authorization_api.php");
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$sender_id = 1;
-$receiver_id = 2;
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+$sender_id  = $data -> sender;
+$receiver_id = $data -> receiver;
+
+$decoded_sender = JWT::decode($sender_id, new Key($key, 'HS256'));
+$decoded_sender = $decoded_sender -> id;
+$decoded_receiver = JWT::decode($receiver_id, new Key($key, 'HS256'));
+$decoded_receiver = $decoded_receiver -> id;
 
 $query = $mysqli -> prepare("DELETE FROM friendships WHERE sender = ? AND receiver = ?");
-$query->bind_param("ii", $receiver_id, $sender_id);
+$query->bind_param("ii", $decoded_receiver, $decoded_sender);
 $query->execute();
 
 $array_response = [];
