f1af32549ea76f8cee272def8e16328dea1f0811
Saad <badreddinesaad91@gmail.com>
2022-02-14 10:22:16 +0200

    API can be fetched using POST; JWT implemented

--- getfriendrequest_api.php
+++ getfriendrequest_api.php
@@ -1,8 +1,21 @@
 <?php
 
 include("db_info.php");
+include("authorization_api.php");
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$user_id = 1;
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+$sender_id  = $data -> sender;
+
+$decoded_sender = JWT::decode($sender_id, new Key($key, 'HS256'));
+$decoded_sender = $decoded_sender -> id;
 
 $query = $mysqli -> prepare("SELECT id, first_name, last_name, picture
 FROM users INNER JOIN friendships ON users.id = friendships.sender OR users.id = friendships.receiver
