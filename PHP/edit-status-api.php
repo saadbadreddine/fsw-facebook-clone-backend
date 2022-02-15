fb8205dc084116ccd494335ae62f001e68ed8515
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-15 21:57:18 +0200

    Change validation and add query to get timestamp

--- PHP/edit-status-api.php
+++ PHP/edit-status-api.php
@@ -2,6 +2,8 @@
 
 include("db_info.php");
 include("authorization_api.php");
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: POST");
@@ -10,6 +12,17 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $json = file_get_contents('php://input');
 $data = json_decode($json);
+$user_id = $data -> user_id;
+
+if(isset($user_id)){
+    $decoded_id = JWT::decode($user_id, new Key($key, 'HS256'));
+    $decoded_id = $decoded_id -> data;
+}else{
+    $idErr = "User not found";
+    $array_response = array("status" => $idErr);
+    $json_response = json_encode($array_response);
+    echo $json_response;
+}
 
 // Edit a status(post)
 
@@ -17,15 +30,26 @@ if(isset($data -> post_id)){
     $post_id = $data -> post_id;
     $updated_post = $data -> post;
 }else{
-    die("Post not found");
+    $postErr = "Post not found";
+    $array_response = array("status" => $postErr);
+    $json_response = json_encode($array_response);
+    echo $json_response;
 }
 
-$query = $mysqli->prepare("UPDATE posts SET post=? WHERE post_id=?");
-$query->bind_param("si", $updated_post, $post_id);
+$query = $mysqli->prepare("UPDATE posts SET post=? WHERE post_id=? AND user_id=?");
+$query->bind_param("sii", $updated_post, $post_id, $decoded_id);
 $query->execute();
 
+$query1 = $mysqli->prepare("SELECT timestamp FROM posts WHERE post_id = ?");
+$query1->bind_param("i", $post_id);
+$query1->execute();
+
+$query1->store_result();
+$query1->bind_result($timestamp);
+$query1->fetch();
+
 $array_response = [];
-$array_response = ["status" => "Status updated", "post" => $updated_post];
+$array_response = ["status" => "Status updated", "post" => $updated_post, "post_id" => $post_id, "timestamp" => $timestamp];
 
 $json_response = json_encode($array_response);
 echo $json_response;
