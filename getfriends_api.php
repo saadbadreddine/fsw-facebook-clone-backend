2d04d05f03efebb9c73db95a546e985df7bceed0
Saad <badreddinesaad91@gmail.com>
2022-02-16 12:11:26 +0200

    Fixed Querry

--- getfriends_api.php
+++ getfriends_api.php
@@ -24,8 +24,9 @@ if (!empty($data->sender)) {
   FROM users INNER JOIN friendships ON users.id = friendships.sender OR users.id = friendships.receiver
   LEFT JOIN blocks ON  users.id = blocks.receiver OR users.id = blocks.sender
   WHERE (friendships.sender = ? OR friendships.receiver = ?) AND friendships.accepted = 1 AND id != ?
-  AND id NOT IN (SELECT blocks.sender FROM blocks WHERE blocks.receiver = ?) ");
-  $query->bind_param("iiii", $decoded_sender, $decoded_sender, $decoded_sender, $decoded_sender);
+  AND id NOT IN (SELECT blocks.sender FROM blocks WHERE blocks.receiver = ?)
+  AND id NOT IN (SELECT blocks.receiver FROM blocks WHERE blocks.sender = ?)");
+  $query->bind_param("iiiii", $decoded_sender, $decoded_sender, $decoded_sender, $decoded_sender, $decoded_sender);
   $query->execute();
 
   $array = $query->get_result();
